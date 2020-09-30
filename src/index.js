a3c4a6888d4c0233d6c599c2fa236a0feb3e214f
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2020-09-30 17:43:15 +0500

    Configure Tictactoe App with Webpack

--- src/index.js
+++ src/index.js
@@ -1,21 +1,99 @@
-import _ from 'lodash';
 import './style.css';
-import Icon from './icon.jpeg';
+const player = (name, symbol) => ({ name, symbol });
 
-function component() {
-  const element = document.createElement('div');
+const board = (() => {
+  let winCheck = true;
+  const boardArray = ['', '', '', '', '', '', '', '', ''];
+  const winArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [3, 5, 7],
+    [1, 5, 9],
+  ];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  const updateBoard = (index, player) => {
+    if (winCheck === true) {
+      boardArray[index] = player.symbol;
+    }
+  };
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  const displayBoard = () => {
+    boardArray.forEach((items, index) => {
+      document.getElementById(`inner-board-${index}`).textContent = `${items}`;
+    });
+  };
 
-  element.appendChild(myIcon);
+  const winStatus = (player) => {
+    winArray.forEach((item) => {
+      let count = 0;
+      item.forEach((char) => {
+        if (boardArray[char - 1] === player.symbol) {
+          count += 1;
+        }
+        if (count === 3) {
+          winCheck = false;
+          document.getElementById('winstatus').textContent = `Congratulations ${player.name}! You Win `;
+        }
+      });
+    });
+  };
 
-  return element;
-}
+  const drawStatus = () => {
+    if (winCheck === true && !boardArray.includes('')) {
+      document.getElementById('winstatus').textContent = 'Game Draw! Try Again.';
+    }
+  };
+  return {
+    boardArray, updateBoard, displayBoard, winStatus, drawStatus,
+  };
+})();
 
-document.body.appendChild(component());
+const playGame = () => {
+  let firstPlayerName;
+  let secondPlayerName;
+  let playerOne;
+  let playerTwo;
+  let currentPlayer;
+  let count = 0;
+  const switchPlayer = () => {
+    if (count === 1) {
+      count -= 1;
+      currentPlayer = playerTwo;
+    } else {
+      count += 1;
+      currentPlayer = playerOne;
+    }
+  };
+
+  document.getElementById('playgame').addEventListener('click', () => {
+    firstPlayerName = document.getElementById('name1').value;
+    secondPlayerName = document.getElementById('name2').value;
+    document.getElementById('outer-board').style.display = 'grid';
+    playerOne = player(firstPlayerName, 'X');
+    playerTwo = player(secondPlayerName, 'O');
+    document.getElementById('playgame').style.display = 'none';
+    document.getElementById('reloadbutton').style.display = 'block';
+  });
+
+  document.getElementById('reloadbutton').addEventListener('click', () => {
+    window.location.reload();
+  });
+
+  const play = (event) => {
+    const index = event.target.getAttribute('data-index');
+    if (board.boardArray[index] === '') {
+      switchPlayer();
+      board.updateBoard(index, currentPlayer);
+      board.displayBoard();
+      board.winStatus(currentPlayer);
+      board.drawStatus();
+    }
+  };
+  document.getElementById('outer-board').addEventListener('click', play);
+};
+
+playGame();
